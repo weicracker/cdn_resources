aecf48d12879d64159ec05a315ec524035ee8272
Ji Wei <jiwei3778@gmail.com>
2024-07-12 16:13:30 +0800

    Update apps_json.js

--- apps_json.js
+++ apps_json.js
@@ -6,17 +6,22 @@ window.APPS_JSON = [
     description: "沃伴新生学生端描述",
     downloadURLs: [
       {
-        url: "https://www.123pan.com/s/j4qmjv-SD3Qh.html",
+        url: "https://cimg.tbkjkf.com:19528/cimg/open-images/dlapp/%E6%B2%83%E4%BC%B4%E6%96%B0%E7%94%9F%E5%AD%A6%E7%94%9F%E7%AB%AF-v1.13.apk",
         btnName: "通用下载",
         bgColor: "#28caad"
       }
+      // {
+      //   url: "https://www.123pan.com/s/j4qmjv-SD3Qh.html",
+      //   btnName: "云盘下载",
+      //   bgColor: "#28caad"
+      // }
     ],
     icon: "https://s21.ax1x.com/2024/07/10/pkfx7p8.png",
     license: "MIT",
     size: "1.0",
-    author: "XXXXXXX有限公司",
-    updateDes: "1.xxxx\n2.xxxx\n3.xxxx",
-    updateTime: "2021-09-01 09:10:00"
+    author: "联通（辽宁）有限公司",
+    updateDes: "1.优化启动速度\n2.系统安全性提升",
+    updateTime: "2024-07-07 09:10:00"
   },
   {
     name: "沃伴新生教师端",
@@ -25,16 +30,21 @@ window.APPS_JSON = [
     description: "沃伴新生教师端描述",
     downloadURLs: [
       {
-        url: "https://www.123pan.com/s/j4qmjv-lD3Qh.html",
+        url: "https://cimg.tbkjkf.com:19528/cimg/open-images/dlapp/%E6%B2%83%E4%BC%B4%E6%96%B0%E7%94%9F%E6%95%99%E5%B8%88%E7%AB%AF-v1.13.apk",
         btnName: "通用下载",
         bgColor: "#28caad"
       }
+      // {
+      //   url: "https://www.123pan.com/s/j4qmjv-lD3Qh.html",
+      //   btnName: "云盘下载",
+      //   bgColor: "#28caad"
+      // }
     ],
     icon: "https://s21.ax1x.com/2024/07/10/pkfxH1S.png",
     license: "MIT",
     size: "1.0",
-    author: "XXXXXXX有限公司",
-    updateDes: "1.xxxx\n2.xxxx\n3.xxxx",
-    updateTime: "2021-09-02 09:10:00"
-  },
+    author: "联通（辽宁）有限公司",
+    updateDes: "1.优化启动速度\n2.系统安全性提升",
+    updateTime: "2024-07-07 09:10:00"
+  }
 ];
