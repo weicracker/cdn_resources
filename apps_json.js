d23ace37848ff4dbd0db0489363bfe26ccca0dcd
Ji Wei <jiwei3778@gmail.com>
2024-07-12 13:30:33 +0800

    Update apps_json.js

--- apps_json.js
+++ apps_json.js
@@ -1,13 +1,13 @@
 window.APPS_JSON = [
   {
-    name: "XXX新生学生端",
+    name: "沃伴新生学生端",
     id: "woban_xs",
     version: "1.0.0",
-    description: "XXX新生学生端描述",
+    description: "沃伴新生学生端描述",
     downloadURLs: [
       {
-        url: "https://github.com/woban-new/woban-new/releases/download/1.0.0/woban-new.zip",
-        btnName: "下载安装",
+        url: "https://jiwei.lanzout.com/iVayA248hwpe",
+        btnName: "通用下载",
         bgColor: "#28caad"
       }
     ],
@@ -19,18 +19,13 @@ window.APPS_JSON = [
     updateTime: "2021-09-01 09:10:00"
   },
   {
-    name: "XXX新生教师端",
+    name: "沃伴新生教师端",
     id: "woban_js",
     version: "1.0.0",
-    description: "XXX新生教师端描述",
+    description: "沃伴新生教师端描述",
     downloadURLs: [
       {
-        url: "https://github.com/woban-new/woban-new/releases/download/1.0.0/woban-new.zip",
-        btnName: "联通下载",
-        bgColor: "#28caad"
-      },
-      {
-        url: "https://github.com/woban-new/woban-new/releases/download/1.0.0/woban-new.zip",
+        url: "https://jiwei.lanzout.com/iUULK248hwla",
         btnName: "通用下载",
         bgColor: "#28caad"
       }
@@ -42,38 +37,4 @@ window.APPS_JSON = [
     updateDes: "1.xxxx\n2.xxxx\n3.xxxx",
     updateTime: "2021-09-02 09:10:00"
   },
-  {
-    name: "诠学",
-    id: "quanxue",
-    version: "1.0.0",
-    description: "诠学描述",
-    downloadURLs: [
-      {
-        url: "https://github.com/woban-new/woban-new/releases/download/1.0.0/woban-new.zip",
-        btnName: "联通下载",
-        bgColor: "#28caad"
-      },
-      {
-        url: "https://github.com/woban-new/woban-new/releases/download/1.0.0/woban-new.zip",
-        btnName: "移动下载",
-        bgColor: "#28caad"
-      },
-      {
-        url: "https://github.com/woban-new/woban-new/releases/download/1.0.0/woban-new.zip",
-        btnName: "电信下载",
-        bgColor: "#28caad"
-      },
-      {
-        url: "https://github.com/woban-new/woban-new/releases/download/1.0.0/woban-new.zip",
-        btnName: "广电下载",
-        bgColor: "#28caad"
-      }
-    ],
-    icon: "https://s21.ax1x.com/2024/07/10/pkfxH1S.png",
-    license: "MIT",
-    size: "1.0",
-    author: "XXXXXXX有限公司",
-    updateDes: "1.xxxx\n2.xxxx\n3.xxxx",
-    updateTime: "2021-09-02 09:10:00"
-  }
 ];
