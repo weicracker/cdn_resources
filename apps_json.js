0cab8ad58fab25ef694470a8e60f03c5cfdcca49
Ji Wei <jiwei3778@gmail.com>
2024-07-12 16:15:59 +0800

    Update apps_json.js

--- apps_json.js
+++ apps_json.js
@@ -2,12 +2,12 @@ window.APPS_JSON = [
   {
     name: "沃伴新生学生端",
     id: "woban_xs",
-    version: "1.0.0",
+    version: "1.13",
     description: "沃伴新生学生端描述",
     downloadURLs: [
       {
         url: "https://cimg.tbkjkf.com:19528/cimg/open-images/dlapp/%E6%B2%83%E4%BC%B4%E6%96%B0%E7%94%9F%E5%AD%A6%E7%94%9F%E7%AB%AF-v1.13.apk",
-        btnName: "通用下载",
+        btnName: "下载安装",
         bgColor: "#28caad"
       }
       // {
@@ -18,7 +18,7 @@ window.APPS_JSON = [
     ],
     icon: "https://s21.ax1x.com/2024/07/10/pkfx7p8.png",
     license: "MIT",
-    size: "1.0",
+    size: "2.8",
     author: "联通（辽宁）有限公司",
     updateDes: "1.优化启动速度\n2.系统安全性提升",
     updateTime: "2024-07-07 09:10:00"
@@ -26,12 +26,12 @@ window.APPS_JSON = [
   {
     name: "沃伴新生教师端",
     id: "woban_js",
-    version: "1.0.0",
+    version: "1.13",
     description: "沃伴新生教师端描述",
     downloadURLs: [
       {
         url: "https://cimg.tbkjkf.com:19528/cimg/open-images/dlapp/%E6%B2%83%E4%BC%B4%E6%96%B0%E7%94%9F%E6%95%99%E5%B8%88%E7%AB%AF-v1.13.apk",
-        btnName: "通用下载",
+        btnName: "下载安装",
         bgColor: "#28caad"
       }
       // {
@@ -42,7 +42,7 @@ window.APPS_JSON = [
     ],
     icon: "https://s21.ax1x.com/2024/07/10/pkfxH1S.png",
     license: "MIT",
-    size: "1.0",
+    size: "2.8",
     author: "联通（辽宁）有限公司",
     updateDes: "1.优化启动速度\n2.系统安全性提升",
     updateTime: "2024-07-07 09:10:00"
