9431a19f68b6b2c10330e70c8be236198094ae26
Ji Wei <jiwei3778@gmail.com>
2024-08-20 15:18:41 +0800

    Update apps_json.js

--- apps_json.js
+++ apps_json.js
@@ -19,7 +19,7 @@ window.APPS_JSON = [
     icon: "https://s21.ax1x.com/2024/07/12/pk4ArwT.png",
     license: "MIT",
     size: "2.8",
-    author: "联通（辽宁）有限公司",
+    author: "联通（辽宁）产业互联网有限公司",
     updateDes: "1.优化启动速度\n2.系统安全性提升",
     updateTime: "2024-07-07 09:10:00"
   },
@@ -43,8 +43,46 @@ window.APPS_JSON = [
     icon: "https://s21.ax1x.com/2024/07/12/pk4ADmV.png",
     license: "MIT",
     size: "2.8",
-    author: "联通（辽宁）有限公司",
+    author: "联通（辽宁）产业互联网有限公司",
     updateDes: "1.优化启动速度\n2.系统安全性提升",
     updateTime: "2024-07-07 09:10:00"
+  },
+  {
+    name: "青程校园",
+    id: "qcxy",
+    version: "1.5.55",
+    description: "青程校园描述",
+    downloadURLs: [
+      {
+        url: "https://cimg.tbkjkf.com:19528/cimg/open-images/dlapp/%E9%9D%92%E7%A8%8B%E6%A0%A1%E5%9B%AD_1.5.55.apk",
+        btnName: "下载安装",
+        bgColor: "#28caad"
+      }
+    ],
+    icon: "https://cimg.tbkjkf.com:19528/cimg/open-images/dlapp/65d7e5ff297bea8b05d0755c83ef5507.jfif",
+    license: "MIT",
+    size: "28",
+    author: "联通（辽宁）产业互联网有限公司",
+    updateDes: "1.优化启动速度\n2.系统安全性提升",
+    updateTime: "2023-07-18 09:10:00"
+  },
+  {
+    name: "桃李校园",
+    id: "tlxy",
+    version: "1.5.5",
+    description: "桃李校园端描述",
+    downloadURLs: [
+      {
+        url: "https://cimg.tbkjkf.com:19528/cimg/open-images/dlapp/%E6%A1%83%E6%9D%8E%E6%A0%A1%E5%9B%AD_1.5.5.apk",
+        btnName: "下载安装",
+        bgColor: "#28caad"
+      }
+    ],
+    icon: "https://cimg.tbkjkf.com:19528/cimg/open-images/dlapp/1c9d990ff13597be920a567395a54134.jfif",
+    license: "MIT",
+    size: "28",
+    author: "联通（辽宁）产业互联网有限公司",
+    updateDes: "1.优化启动速度\n2.系统安全性提升",
+    updateTime: "2023-07-18 09:10:00"
   }
 ];
